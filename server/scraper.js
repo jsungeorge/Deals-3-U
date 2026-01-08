4818da624c6b4abccb5da8ef45c0e534334ff9bf
george <george@eduroam-campus-10-36-154-123.campus-dynamic.uwaterloo.ca>
2026-01-08 13:53:26 -0500

    Force remove localhost

--- server/scraper.js
+++ server/scraper.js
@@ -18,6 +18,7 @@ async function scrapeAmazon(url) {
       '--disable-accelerated-2d-canvas',
       '--no-first-run',
       '--no-zygote',
+      '--single-process',
       '--disable-gpu'
     ]
   });
