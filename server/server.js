2832949b85116beb5c00d484de254b9a3951dc31
george <george@eduroam-campus-10-36-13-190.campus-dynamic.uwaterloo.ca>
2026-01-09 18:37:37 -0500

    Switch to SMTP port 587

--- server/server.js
+++ server/server.js
@@ -24,7 +24,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 // EMAIL CONFIGURATION
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,            
+  secure: false,       
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
