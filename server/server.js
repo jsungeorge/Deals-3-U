8651a8a4e3c3f8f31df653fdae21253ec349c8c1
george <george@georges-MacBook-Pro.local>
2026-01-14 00:31:57 -0500

    Email fix

--- server/server.js
+++ server/server.js
@@ -23,22 +23,6 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error('❌ MongoDB Error:', err));
 
 // EMAIL CONFIGURATION
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-
-  family: 4,           
-  connectionTimeout: 10000, 
-  greetingTimeout: 5000,   
-  logger: true,       
-  debug: true          
-});
-
 const sendAlertEmail = async (userEmail, product) => {
   console.log(`Attempting to email ${userEmail}...`);
 
